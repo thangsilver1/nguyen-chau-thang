713b32c5512367d65aab2b70380fe983745d973c
thangsilver1 <46095886+thangsilver1@users.noreply.github.com>
2018-12-24 14:44:56 +0700

    Update source_code.cpp

--- BAITAP/BAI_1/source_code.cpp
+++ BAITAP/BAI_1/source_code.cpp
@@ -1,1 +1,29 @@
 // code bai 1
+#include <iostream>
+#include <fstream>
+#include <string.h>
+	bool GioiTinh;
+
+using namespace std;
+
+fstream f;
+
+struct ThongTin{
+	char ten[10];
+	bool GioiTinh;
+	char SoDienThoai[10];
+	char Email[50];
+	char DiaChi[100];
+};
+
+void Them(ThongTin tt){
+	f.open("DU_LIEU/DANH_BA.txt", ios::out);
+	f << "abc\n";
+	f << "xyz\n";
+	f.close();
+}
+
+int main(){
+	ThongTin tt;
+	Them(tt);
+}
