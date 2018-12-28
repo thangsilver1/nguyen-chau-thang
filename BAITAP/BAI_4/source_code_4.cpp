8c2ad3bc5d15fe1517c841d9508a2020584d4b2b
thangsilver1 <46095886+thangsilver1@users.noreply.github.com>
2018-12-28 10:47:48 +0700

    update

--- BAITAP/BAI_4/source_code_4.cpp
+++ BAITAP/BAI_4/source_code_4.cpp
@@ -4,6 +4,10 @@
 
 using namespace std;
 
+struct BienLuu{
+	int ten;
+	int tong;
+};
 // --- doc ma tran trong file ra mang 2 chieu ---
 void MoDoThi(int a[6][6]){
 	fstream f;
@@ -23,10 +27,34 @@ void XuatMaTran(int a[6][6]){
 	}		
 }
 
+void TimDuong(int a[6][6]){
+	BienLuu LD[6];
+	int i = 0;
+	int n = 0;
+	for(int l = 0; l < 6; l++){
+		LD[l].ten = l;
+		LD[l].tong = 0;
+	}
+	while(n <= 5){
+		n++;
+		for(int j = 0; j < 6; j++)
+			if(a[i][j] != 0){
+				LD[j].ten = j;
+				LD[j].tong = LD[j].tong + a[i][j];
+			}
+		i++;
+	}
+	for(int x = 0; x < 6; x++)
+		cout << LD[x].ten << "\t" << LD[x].tong << endl;
+}
 int main(){
 	int a[6][6];
 	
 	MoDoThi(a);
 	
 	XuatMaTran(a);
+	
+	cout << endl;
+	
+	TimDuong(a);
 }
