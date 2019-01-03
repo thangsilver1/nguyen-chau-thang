935a0359b1615cffba32f10549ee0a15bb531bcc
thangsilver1 <46095886+thangsilver1@users.noreply.github.com>
2019-01-03 18:49:22 +0700

    update

---
+++ THUC_TAP_NHOM_THAY_THUAN/BAI_3/source_code_3.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+
+using namespace std;
+
+struct AVL{
+	int gt;
+	AVL *tr;
+	AVL *ph;
+};
+
+// --- tao goc cho cay AVL ---
+AVL *goc;
+void Tao(AVL *&goc){
+	goc = NULL;
+}
+
+// --- ham xac dinh yeu to can bang cua 1 nut ---
+int NutCanBang(){
+	
+}
+
+// --- moc 1 nut vao cay ---
+void Moc(AVL *&goc, int x){
+	if(goc == NULL){
+		goc = new AVL;
+		goc->gt = x;
+		goc->ph = NULL;
+		goc->tr = NULL;
+	}
+	else
+		if(x >= goc->gt)
+			Moc(goc->ph, x);
+		else
+			Moc(goc->tr, x);
+}
+
+// --- nhap cay ---
+void NhapCay(AVL *&goc){
+	int x;
+	do{
+		cout << "nhap gia tri: ";
+		cin >> x;
+		if(x != 0)
+			Moc(goc, x);
+	}while(x != 0);
+}
+
+// --- xuat cay ---
+void XuatCay(AVL *goc){
+	if(goc != NULL){
+		XuatCay(goc->ph);
+		cout << "\t" << goc->gt;
+		XuatCay(goc->tr);
+	}	
+}
+
+// --- dem so la cua cay ---
+int DemLa(AVL *goc){
+	if(goc == NULL)
+		return 0;
+	else
+		if(goc->ph == NULL && goc->tr == NULL)
+			return 1;
+		else
+			return DemLa(goc->tr) + DemLa(goc->ph);
+}
+
+// --- tim vi tri cua nut mang gia tri x ---
+int TimVT(AVL *goc, int x){
+	if(goc != NULL)
+		if(x == goc->gt)
+			return 1;
+		else
+			if(x < goc->gt)
+				return TimVT(goc->tr, x)*2;
+			else
+				return TimVT(goc->ph, x)*2 + 1;
+}
+
+// --- in chieu cao cay ---
+int ChieuCao(AVL *goc){
+	if(goc == NULL)
+		return 0;
+	else
+		return 1 + max(ChieuCao(goc->tr), ChieuCao(goc->ph));
+}
+
+// --- dem so nut cua cay ---
+int DemNut(AVL *goc){
+	if(goc == NULL)
+		return 0;
+	else
+		return 1 + DemNut(goc->tr) + DemNut(goc->ph);
+}
+
+// --- in cac gia tri tai muc i ---
+void InMuc(AVL *goc, int i){
+	if(goc != NULL)
+		if(i == 1)
+			cout << "\t" << goc->gt;
+		else{
+			i = i - 1;
+			InMuc(goc->tr, i);
+			InMuc(goc->ph, i);
+		}
+}
+
+// --- tong cac nut o muc i ---
+int TongMuc(AVL *goc, int i){
+	if(goc == NULL)
+		return 0;
+	else
+		if(i == 1)
+			return goc->gt;
+		else{
+			i = i - 1;
+			return TongMuc(goc->tr, i) + TongMuc(goc->ph, i);
+		}
+}
+
+
+int main(){
+
+	Tao(goc);
+	NhapCay(goc);
+	XuatCay(goc);
+
+	cout << endl << "so la cua cay: " << DemLa(goc);
+	cout << endl << "vi tri nut mang gia tri x: " << TimVT(goc, 6);
+	cout << endl << "chieu cao cay: " << ChieuCao(goc);
+	cout << endl << "so nut cua cay: " << DemNut(goc);
+	cout << endl << "cac gia tri tai muc i: ";
+	InMuc(goc, 3);
+	cout << endl << "tong cac gia tri tai muc i: " << TongMuc(goc, 3);
+}
