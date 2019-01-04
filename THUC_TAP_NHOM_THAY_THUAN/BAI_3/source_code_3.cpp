1bd6550eed07be50d7879c2815a2bd7fbffff4c5
thangsilver1 <46095886+thangsilver1@users.noreply.github.com>
2019-01-04 17:18:31 +0700

    update

--- THUC_TAP_NHOM_THAY_THUAN/BAI_3/source_code_3.cpp
+++ THUC_TAP_NHOM_THAY_THUAN/BAI_3/source_code_3.cpp
@@ -4,144 +4,190 @@ using namespace std;
 
 struct AVL{
 	int gt;
-	int cb;
+	int cb;   // --- yeu to can bang: (-1 ,0 ,1) can bang / 2 lech trai / -2 lech phai ---
 	AVL *tr;
 	AVL *ph;
 };
 
-// --- tao goc cho cay AVL ---
 AVL *goc;
-void Tao(AVL *&goc){
-	goc = NULL;
-}
 
-// --- ham xac dinh yeu to can bang cua 1 nut ---
-int NutCanBang(AVL *goc){
-	if(goc->tr == NULL && goc->ph == NULL)
-		return 0;
-	else
-		if(goc->tr != NULL && goc->ph == NULL)
-			return 1 + NutCanBang(goc->tr);
-		else
-			if(goc->tr == NULL && goc->ph != NULL)
-				return 1 + NutCanBang(goc->ph);
-			else
-				if(goc->tr != NULL && goc->ph != NULL)
-					return (1 + NutCanBang(goc->tr)) - (1 + NutCanBang(goc->ph));
-}
-
-// --- moc 1 nut vao cay ---
-void Moc(AVL *&goc, int x){
-	if(goc == NULL){
-		goc = new AVL;
-		goc->gt = x;
-		goc->ph = NULL;
-		goc->tr = NULL;
-	}
-	else
-		if(x >= goc->gt)
-			Moc(goc->ph, x);
-		else
-			Moc(goc->tr, x);
-}
-
-// --- nhap cay ---
-void NhapCay(AVL *&goc){
-	int x;
-	do{
-		cout << "nhap gia tri / nhap 0 de ket thuc nhap: ";
-		cin >> x;
-		if(x != 0)
-			Moc(goc, x);
-	}while(x != 0);
-}
-
-// --- xuat cay ---
-void XuatCay(AVL *goc){
-	if(goc != NULL){
-		XuatCay(goc->ph);
-		cout << goc->gt << "\t";
-		XuatCay(goc->tr);
-	}
-}
-
-// --- dem so la cua cay ---
-int DemLa(AVL *goc){
-	if(goc == NULL)
-		return 0;
-	else
-		if(goc->ph == NULL && goc->tr == NULL)
-			return 1;
-		else
-			return DemLa(goc->tr) + DemLa(goc->ph);
-}
-
-// --- tim vi tri cua nut mang gia tri x ---
-int TimVT(AVL *goc, int x){
-	if(goc != NULL)
-		if(x == goc->gt)
-			return 1;
-		else
-			if(x < goc->gt)
-				return TimVT(goc->tr, x)*2;
-			else
-				return TimVT(goc->ph, x)*2 + 1;
-}
-
-// --- in chieu cao cay ---
-int ChieuCao(AVL *goc){
-	if(goc == NULL)
-		return 0;
-	else
-		return 1 + max(ChieuCao(goc->tr), ChieuCao(goc->ph));
-}
-
-// --- dem so nut cua cay ---
-int DemNut(AVL *goc){
-	if(goc == NULL)
-		return 0;
-	else
-		return 1 + DemNut(goc->tr) + DemNut(goc->ph);
-}
-
-// --- in cac gia tri tai muc i ---
-void InMuc(AVL *goc, int i){
-	if(goc != NULL)
-		if(i == 1)
-			cout << "\t" << goc->gt;
-		else{
-			i = i - 1;
-			InMuc(goc->tr, i);
-			InMuc(goc->ph, i);
-		}
-}
-
-// --- tong cac nut o muc i ---
-int TongMuc(AVL *goc, int i){
-	if(goc == NULL)
-		return 0;
-	else
-		if(i == 1)
-			return goc->gt;
-		else{
-			i = i - 1;
-			return TongMuc(goc->tr, i) + TongMuc(goc->ph, i);
-		}
-}
+void Tao(AVL *&goc);   // --- tao goc cho cay AVL ---
+void TaoCay(AVL *&goc, AVL *nut);   // --- tao dinh cua cay ---
+void NhapNut(AVL *&goc);   // --- nhap cay ---
+void XuatCay(AVL *goc);   // --- xuat cay ---
+int ChieuCao(AVL *nut);   // --- chieu cao cay tai 1 nut ---
+void NutCanBang(AVL *goc);   // --- ham xac dinh yeu to can bang cua 1 nut / gan cho nut luon ---
+void KiemTraCB(AVL *&goc);
+void XoayTrTr(AVL *&nut);   // --- hieu chinh trai trai---
+void XoayTrPh(AVL *&nut);   // --- hieu chinh trai phai ---
+void XoayPhPh(AVL *&nut);   // --- hieu chinh phai phai ---
+void XoayPhTr(AVL *&nut);   // --- hieu chinh phai trai ---
+void XuLyXoay(AVL *&nut);   // --- ham xu ly cac hieu chinh xoay ---
 
 
 int main(){
 
 	Tao(goc);
-	NhapCay(goc);
+	
+	NhapNut(goc);
+	
+//	NutCanBang(goc);
+	
 	XuatCay(goc);
 	
-	cout << endl << NutCanBang(goc);
-//	cout << endl << "so la cua cay: " << DemLa(goc);
-//	cout << endl << "vi tri nut mang gia tri x: " << TimVT(goc, 6);
-//	cout << endl << "chieu cao cay: " << ChieuCao(goc);
-//	cout << endl << "so nut cua cay: " << DemNut(goc);
-//	cout << endl << "cac gia tri tai muc i: ";
-//	InMuc(goc, 3);
-//	cout << endl << "tong cac gia tri tai muc i: " << TongMuc(goc, 3);
 }
+
+// --- cac ham xu ly ---
+
+void Tao(AVL *&goc){
+	goc = NULL;
+}
+
+void TaoCay(AVL *&goc, AVL *nut){
+	if(goc == NULL){
+		nut->tr = NULL;
+		nut->ph = NULL;
+		goc = nut;
+	}
+	else{
+		if(nut->gt <= goc->gt)
+			TaoCay(goc->tr, nut);
+		else
+			TaoCay(goc->ph, nut);
+	}
+}
+
+void NhapNut(AVL *&goc){
+	AVL *nut;
+	do{
+		nut = new AVL;
+		cout << "nhap gia tri / nhap 0 de ket thuc nhap: ";
+		cin >> nut->gt;
+		if(nut->gt != 0){
+			TaoCay(goc, nut);
+			NutCanBang(goc);
+			KiemTraCB(goc);
+			NutCanBang(goc);
+		}
+	}while(nut->gt != 0);
+}
+
+void XuatCay(AVL *goc){
+	if(goc != NULL){
+		XuatCay(goc->tr);
+		cout << goc->gt << "," << goc->cb << "\t";
+		XuatCay(goc->ph);
+	}
+}
+
+int ChieuCao(AVL *nut){
+	if(nut == NULL)
+		return 0;
+	else
+		return 1 + max(ChieuCao(nut->tr), ChieuCao(nut->ph));
+}
+
+void KiemTraCB(AVL *&goc){
+	AVL *x;
+	if(goc->cb == 2){
+		x = goc->tr;
+		if(x->cb == 2)
+			KiemTraCB(goc->tr);
+		else{
+			x = goc;
+			XuLyXoay(x);
+			goc = x;
+		}
+	}
+	else{
+		if(goc->cb == -2){
+			x = goc->ph;
+			if(x->cb == -2)
+				KiemTraCB(goc->ph);
+			else{
+				x = goc;
+				XuLyXoay(x);
+				goc = x;
+			}
+		}
+	}
+}
+
+void NutCanBang(AVL *goc){
+	if(goc != NULL){
+		goc->cb = ChieuCao(goc->tr) - ChieuCao(goc->ph);
+		NutCanBang(goc->tr);
+		NutCanBang(goc->ph);
+	}
+}
+
+void XoayTrTr(AVL *&nut){
+	AVL *a, *b, *du;
+	a = nut;
+	b = nut->tr;
+	du = b->ph;
+	b->ph = a;
+	a->tr = du;
+	nut = b;
+}
+
+void XoayTrPh(AVL *&nut){
+	AVL *a, *b, *c ,*ct, *cp;
+	a = nut;
+	b = nut->tr;
+	c = b->ph;
+	ct = c->tr;
+	cp = c->ph;
+	b->ph = ct;
+	c->tr = b;
+//	a->tr = c;
+	a->tr = cp;
+	c->ph = a;
+	nut = c;
+}
+
+void XoayPhPh(AVL *&nut){
+	AVL *a, *b, *du;
+	a = nut;
+	b = nut->ph;
+	du = b->tr;
+	b->tr = a;
+	a->ph = du;
+	nut = b;
+}
+
+void XoayPhTr(AVL *&nut){
+	AVL *a, *b, *c ,*ct, *cp;
+	a = nut;
+	b = nut->ph;
+	c = b->tr;
+	ct = c->tr;
+	cp = c->ph;
+	b->tr = cp;
+	c->ph = b;
+//	a->ph = c;
+	a->ph = ct;
+	c->tr = a;
+	nut = c;
+}
+
+void XuLyXoay(AVL *&nut){
+	AVL *kt;
+	if(nut != NULL){
+		if(nut->cb == 2){
+			kt = nut->tr;
+			if(kt->tr != NULL)
+				XoayTrTr(nut);
+			else
+				XoayTrPh(nut);
+		}
+		if(nut->cb == -2){
+			kt = nut->ph;
+			if(kt->tr != NULL)
+				XoayPhTr(nut);
+			else
+				XoayPhPh(nut);
+		}
+	}
+}
+
